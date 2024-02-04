58fcbeed4b2160969aa852c4b781e4a5036a23a2
Nacchofer31 <ignaciofer31@gmail.com>
2024-02-04 19:25:31 +0100

    Updated build url variables

--- buildSrc/src/main/kotlin/HostUrl.kt
+++ buildSrc/src/main/kotlin/HostUrl.kt
@@ -1,14 +1,14 @@
 object HostUrl {
 
     object DEBUG {
-        const val HOST = "<<DEBUG_URL>>"
+        const val HOST = "DEBUG_URL"
     }
 
     object STAGE {
-        const val HOST = "<<STAGE_URL>>"
+        const val HOST = "STAGE_URL"
     }
 
     object RELEASE {
-        const val HOST = "<<RELEASE_URL>>"
+        const val HOST = "RELEASE_URL"
     }
 }
